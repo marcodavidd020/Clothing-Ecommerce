a0b77cc7613258627e91e3cf4551f884b49197a1
marcodavidd020 <marcodaviddtc@gmail.com>
2025-05-14 08:38:10 -0400

    feat(dependencies): update and add new packages
    - ✨ Add `mockito` dependency to facilitate unit testing
    - 🔄 Update `build_runner` to version ^2.4.9 for improved code generation
    - 🧹 Remove temporary `AuthLocalDataSource` implementations to simplify codebase
    - 📦 Add multiple new dependencies in `pubspec.lock` to enhance project functionality

--- lib/features/auth/data/models/request/sign_in_params.dart
+++ lib/features/auth/data/models/request/sign_in_params.dart
@@ -5,9 +5,6 @@ class SignInParams {
   SignInParams({required this.email, required this.password});
 
   Map<String, dynamic> toJson() {
-    return {
-      'email': email,
-      'password': password,
-    };
+    return {'email': email, 'password': password};
   }
-} 
+}
